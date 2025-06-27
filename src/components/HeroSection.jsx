a7904d5fa1bd458fd10985140804cf7d97e6def7
Ayush <heyayush87@gmail.com>
2025-06-27 12:13:32 +0530

    made changes in herosection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -31,16 +31,25 @@ const HeroSection = () => {
             }}
             className="text-xl md:text-1xl lg:text-2xl text-purple-200 max-w-2xl"
           >
-            Hi, I'm Ayush Kumar — a passionate Frontend Developer with a growing
-            interest in full-stack development. I’ve previously worked at
-            Zintlr, a B2B SaaS company, 
+            Hi, I'm Ayush Kumar — a Frontend Developer passionate about crafting
+            elegant user interfaces and interactive experiences. I’ve built
+            dynamic platforms like <strong>NetStream</strong>, an AI-powered
+            movie recommendation system using{" "}
+            <strong>Gemini AI, React.js, Redux Toolkit</strong>, and{" "}
+            <strong>Firebase</strong>, and
+            <strong>FoodVilla</strong>, a live food ordering interface powered
+            by the Swiggy API. My portfolio showcases immersive 3D design using{" "}
+            <strong>Spline</strong>, fluid transitions with{" "}
+            <strong>Framer Motion</strong>, and custom animations using{" "}
+            <strong>GSAP</strong>. I’m dedicated to building performant,
+            mobile-friendly UIs that combine beauty with scalability.
           </motion.p>
         </div>
         {/*Right Section */}
         <Spline
           className="absolute xl:right-[-28%] right-0 top-[-20%]  lg:top-0"
-          scene="https://prod.spline.design/G06g951NSyTG8Kax/scene.splinecode" />
-        
+          scene="https://prod.spline.design/G06g951NSyTG8Kax/scene.splinecode"
+        />
       </section>
     );
 }
